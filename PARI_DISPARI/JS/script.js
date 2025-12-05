aceba8c02b8842db6c38c24e18779a777ce9027f
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-05 17:00:57 +0100

    Esercizio PARI_DISPARI  scelt apari o dispari ed inserimento numeri iniziali

--- PARI_DISPARI/JS/script.js
+++ PARI_DISPARI/JS/script.js
@@ -5,3 +5,31 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
+//chiamiamo l'utente a scegliere pari o dispari
+let pariDispari = prompt('Scegli se il risutalto finale sarà pari o dispari')
+while (pariDispari !== 'pari' && pariDispari !== 'dispari' && pariDispari !== 'Pari' && pariDispari !== 'Dispari') {
+    alert('Scelta non valida! Ritenta.');
+    pariDispari = prompt('Scrivi solo "pari" o "dispari"');
+}    
+//chiamiamo l'utente a scegliere un numero da 1 a 5
+let numUser = prompt('Scegli un numero da 1 a 5')
+while (numUser < 1 || numUser > 5){
+    alert('Scelta non valida! Ritenta.');
+    numUser = prompt('Puoi scegliere solo un numero da 1 a 5')
+}
+//genero un numero random da 1 a 5 per il computer
+numComputer = genNumRandInRange(1,5);
+console.log(numComputer);
+
+
+
+
+
+
+
+
+
+//creo una funzione che genera un numero random da 1 5
+function genNumRandInRange(min,max){
+    return Math.floor(Math.random() * (max -min)) + min;
+}
