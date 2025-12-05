b4e6b1bebb1180e3af4cbe33678449c525745e75
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-05 15:17:22 +0100

    Esercizio PALINDROMO sistemato in modo che funzioni anche con maiuscole e minuscole mischiate

--- PALINDROMO/JS/script.js
+++ PALINDROMO/JS/script.js
@@ -14,13 +14,16 @@ function reverseWord(string){
     let verifier = false;
     //assegnamo una variabile per verificare o smentire il risultato
     let palindrome = '';
+    //trasformiamo la stringa in minuscolo per non dare problemi con eventuali lettere maiuscole o miuscole
+    let lowerCaseString = string.toLowerCase()
+    console.log(lowerCaseString)
     //facciamo partire un ciclo che parte dal massimo della lunghezza della parola fino a 0
     for (i = string.length -1; i >= 0; i--){
         //a ogni giro facciamo aggiungiamo la lettera che corrisponde all'indice
-        reverseString = reverseString + string[i]
+        reverseString = reverseString + lowerCaseString[i]
     }
     //andiamo a verificare se la parola ribaltata sia uguala alla parola non ribaltata
-    if (reverseString === word){
+    if (reverseString === lowerCaseString){
         palindrome = true;
     }
     //andiamo ad assegnare la dicitura in caso la parola sia palindroma
@@ -34,6 +37,4 @@ function reverseWord(string){
     //andiamo a far ritornare il risultare
     return palindrome;
 }
-
-
 
